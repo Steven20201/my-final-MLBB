decd77d9e642b32ea892b10075c1cd934cc5d6f8
Steven20201 <khantzawhein261@example.com>
2022-03-25 12:12:50 +0630

    first comit

--- js/script.js
+++ js/script.js
@@ -9,29 +9,36 @@ $(document).ready(function(){
 
         if(attr == "tank")  {
             $("."+attr).show();
+            document.getElementById("myInput").style.display = "none";
         }
 
         else if(attr == "fighter")  {
             $("."+attr).show();
+            document.getElementById("myInput").style.display = "none";
         }
 
         else if(attr == "assassin")  {
             $("."+attr).show();
+            document.getElementById("myInput").style.display = "none";
         }
 
         else if(attr == "mage")  {
             $("."+attr).show();
+            document.getElementById("myInput").style.display = "none";
         }
 
         else if(attr == "marksman")  {
             $("."+attr).show();
+            document.getElementById("myInput").style.display = "none";
         }
 
         else if(attr == "support")  {
             $("."+attr).show();
+            document.getElementById("myInput").style.display = "none";
         }
         else {
             $(".item").show();
+            document.getElementById("myInput").style.display = "block";
         }
     });
 });
